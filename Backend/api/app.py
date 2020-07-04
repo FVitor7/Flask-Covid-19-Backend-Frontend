4873b98a96adfee8961a2c9b2b5df9415377fe53
Fábio Vitor Coelho Silva <48036134+FVitor7@users.noreply.github.com>
2020-07-04 06:19:16 -0300

    Update app.py

--- Backend/api/app.py
+++ Backend/api/app.py
@@ -1,8 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask, render_template,request
 from flask import jsonify
 from flask_cors import CORS, cross_origin
 import urllib.request
 import json,os
+from re import sub
+from decimal import Decimal
 
 app = Flask(__name__)
 
@@ -14,162 +18,81 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin()
 
 def load_api():
-    link = "https://github.com/microsoft/Bing-COVID-19-Data/blob/master/data/Bing-COVID19-Data.csv?raw=true"
-    with urllib.request.urlopen(link) as response:
-        json_data = response.read().decode("utf-8")
-        _bingdata = json.loads(json_data)
 
-    world_cases = _bingdata['totalConfirmed']
-    world_deaths = _bingdata['totalDeaths']
-    world_recovered = _bingdata['totalRecovered']
-    world_lastupdated= _bingdata['lastUpdated']
-
-    # find brazil
-    for index, j in enumerate(_bingdata['areas']):
-        if j['id'] == 'brazil':
-  
-            brazil_cases = _bingdata['areas'][index]['totalConfirmed']
-            
-            brazil_deaths = _bingdata['areas'][index]['totalDeaths']
-            
-            brazil_recovered = _bingdata['areas'][index]['totalRecovered']
-            
-            bahia_cases = _bingdata['areas'][index]['areas'][8]['totalConfirmed']
-          
-            bahia_deaths = _bingdata['areas'][index]['areas'][8]['totalDeaths']
-            
-            bahia_recovered = _bingdata['areas'][index]['areas'][8]['totalRecovered']
-          
-            pernambuco_cases = _bingdata['areas'][index]['areas'][12]['totalConfirmed']
-            
-            pernambuco_deaths = _bingdata['areas'][index]['areas'][12]['totalDeaths']
-           
-            pernambuco_recovered = _bingdata['areas'][index]['areas'][12]['totalRecovered']
-          
-            saopaulo_cases = _bingdata['areas'][index]['areas'][19]['totalConfirmed']
-           
-            saopaulo_deaths = _bingdata['areas'][index]['areas'][19]['totalDeaths']
-            
-            saopaulo_recovered = _bingdata['areas'][index]['areas'][19]['totalRecovered']
-           
-            riodejaneiro_cases = _bingdata['areas'][index]['areas'][18]['totalConfirmed']
-            #print(saopaulo_cases)
-            riodejaneiro_deaths = _bingdata['areas'][index]['areas'][18]['totalDeaths']
-            riodejaneiro_recovered = _bingdata['areas'][index]['areas'][18]['totalRecovered']
-            
-            break
-            
-    _calc = lambda x, y: round(((x * 100) / y), 2)
+	url_world = 'https://www.worldometers.info/coronavirus/'
+	r_world = requests.get(url_world)
+	s_world = BeautifulSoup(r_world.text, "html.parser")
+	data_world = s_world.find_all("div", class_ = "maincounter-number")
+	
+	
+	url_brazil = 'https://www.worldometers.info/coronavirus/country/brazil/'
+	r_brazil = requests.get(url_brazil)
+	s_brazil = BeautifulSoup(r_brazil.text, "html.parser")
+	data_brazil = s_brazil.find_all("div", class_ = "maincounter-number")
+	
+	w0 = data_world[0].text.strip()
+	#w0 = float(w0.replace(",","."))
+	w0_ = Decimal(sub(r'[^\d.]', '', w0))
+	
+	
+	w1 = data_world[1].text.strip()
+	w1_ = Decimal(sub(r'[^\d.]', '', w1))
+	#w1 = float(w1.replace(",","."))
+	
+	w2 = data_world[2].text.strip()
+	#w2 = float(w2.replace(",","."))
+	w2_ = Decimal(sub(r'[^\d.]', '', w2))
+	
+	b0 = data_brazil[0].text.strip()
+	#b0 = float(b0.replace(",","."))
+	b0_ = Decimal(sub(r'[^\d.]', '', b0))
+	print(b0_)
+	
+	b1 = data_brazil[1].text.strip()
+	#b1 = float(b1.replace(",","."))
+	b1_ = Decimal(sub(r'[^\d.]', '', b1))
+	
+	b2 = data_brazil[2].text.strip()
+	#b2 = float(b2.replace(",","."))
+	b2_ = Decimal(sub(r'[^\d.]', '', b2))
+	print(b2_)
+	
+	_calc = lambda x, y: round(((x * 100) / y), 2)
+	
+	world_death_rate = _calc(w1_, w2_)
+	
+	brazil_death_rate = _calc(b1_, b2_)
+	
+	world_recovered_rate = _calc(w2_, w0_)
+	
+	brazil_recovered_rate = _calc(b2_, b0_)
+    
+	
+	
+	
+	world_data_set = {"COVID19Cases":
+      w0, "Deaths": w1,
+      "Recoveries": w2,
+      "DeathRate": str(world_death_rate),
+      "RecoveredRate": str(world_recovered_rate)}
+       #"LastUpdated":world_lastupdated}
+        
+	brazil_data_set = {"COVID19Cases":
+    	b0, "Deaths": b1,
+    	"DeathRate": str(brazil_death_rate),
+    	"Recoveries": b2,
+    	"RecoveredRate": str(brazil_recovered_rate)}
     	
-    world_death_rate = _calc(world_deaths, world_cases)
-    brazil_death_rate = _calc(brazil_deaths, brazil_cases)
-    if bahia_deaths is None:
-    	bahia_death_rate = '-'
-    	bahia_deaths = '-'
-    else:
-    	bahia_death_rate = _calc(bahia_deaths, bahia_cases)
-   
-    if pernambuco_deaths is None:
-    	pernambuco_death_rate = '-'
-    	pernambuco_deaths = '-'
-    else:
-    	pernambuco_death_rate = _calc(pernambuco_deaths, pernambuco_cases)
-
-    if saopaulo_deaths is None:
-    	saopaulo_death_rate = '-'
-    	saopaulo_deaths = '-'
-    else:
-    	saopaulo_death_rate = _calc(saopaulo_deaths, saopaulo_cases)    
-
-    if riodejaneiro_deaths is None:
-    	riodejaneiro_death_rate = '-'
-    	riodejaneiro_deaths = '-'
-    else:
-    	riodejaneiro_death_rate = _calc(riodejaneiro_deaths, riodejaneiro_cases)
-
-    world_recovered_rate = _calc(world_recovered, world_cases)
-    brazil_recovered_rate = _calc(brazil_recovered, brazil_cases)
-    
-    if bahia_recovered is None:
-    	bahia_recovered_rate = '-'
-    	bahia_recovered = '-'
-    else:
-    	bahia_recovered_rate = _calc2(bahia_recovered, bahia_cases)
-    
-    if pernambuco_recovered is None:
-    	pernambuco_recovered_rate = '-'
-    	pernambuco_recovered = '-'
-    else:
-    	pernambuco_recovered_rate = _calc2(pernambuco_recovered, pernambuco_cases) 
-    	
-    if saopaulo_recovered is None:
-    	saopaulo_recovered_rate = '-'
-    	saopaulo_recovered = '-'
-    else:
-    	saopaulo_recovered_rate = _calc2(saopaulo_recovered, saopaulo_cases)
-    	
-    if riodejaneiro_recovered is None:
-    	riodejaneiro_recovered_rate = '-'
-    	riodejaneiro_recovered = '-'
-    else:
-    	riodejaneiro_recovered_rate = _calc2(riodejaneiro_recovered, riodejaneiro_cases)  
-    			    			
-    world_data_set = {"COVID19Cases":
-      world_cases, "Deaths": world_deaths,
-      "DeathRate": world_death_rate,
-      "Recoveries": world_recovered,"RecoveredRate":
-        world_recovered_rate, "LastUpdated":world_lastupdated}
-    
-    brazil_data_set = {"COVID19Cases":
-    	brazil_cases, "Deaths": brazil_deaths,
-    	"DeathRate": brazil_death_rate,
-    	"Recoveries": brazil_recovered,
-    	"RecoveredRate": brazil_recovered_rate}
-  
-    bahia_data_set = {"COVID19Cases":
-    	bahia_cases, "Deaths": bahia_deaths,
-    	"DeathRate": bahia_death_rate,
-    	"Recoveries": bahia_recovered,
-    	"RecoveredRate": bahia_recovered_rate}
-   
-    pernambuco_data_set = {"COVID19Cases":
-    	pernambuco_cases, "Deaths":
-    		pernambuco_deaths, "DeathRate":
-    			pernambuco_death_rate,
-    			"Recoveries": pernambuco_recovered,
-    			"RecoveredRate":
-    				pernambuco_recovered_rate}
-    
-    saopaulo_data_set = {"COVID19Cases":
-		saopaulo_cases, "Deaths":
-			saopaulo_deaths, "DeathRate":
-				saopaulo_death_rate, "Recoveries":
-					saopaulo_recovered,
-					"RecoveredRate":
-						saopaulo_recovered_rate}
-    
-    riodejaneiro_data_set = {"COVID19Cases":
-		riodejaneiro_cases, "Deaths":
-			riodejaneiro_deaths, "DeathRate":
-				riodejaneiro_death_rate, "Recoveries":
-					riodejaneiro_recovered,
-					"RecoveredRate":
-						riodejaneiro_recovered_rate}
-    
-    corona_array = {
+	corona_array = {
         "World": world_data_set,
-        "Brazil": brazil_data_set,
-        "SaoPaulo": saopaulo_data_set,
-        "RiodeJaneiro": riodejaneiro_data_set,
-        "Bahia": bahia_data_set,
-        "Pernambuco": pernambuco_data_set,      
+        "Brazil": brazil_data_set
     }
     
-    return jsonify(COVID19=corona_array)
-    #return render_template('index.html')
+	return jsonify(COVID19=corona_array)
+	
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = float(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
     #app.run()
